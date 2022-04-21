404a199b769b4937a6076c6151bc2d39845e85bd
ldj <dongjae.lee@sf.snu.ac.kr>
2022-04-21 21:41:40 +0900

    showed ex1

---
+++ src/FairBeh.v
@@ -0,0 +1,200 @@
+From sflib Require Import sflib.
+From ITree Require Export ITree.
+From Paco Require Import paco.
+
+Export ITreeNotations.
+
+Require Import Coq.Classes.RelationClasses.
+
+Set Implicit Arguments.
+
+
+Section EVENT.
+  Variable Ident: Type.
+
+  Variant eventE: Type -> Type :=
+    | Choose (X: Type): eventE X
+    | Success (i: Ident): eventE unit
+    | Fail (i: Ident): eventE unit
+  .
+End EVENT.
+
+Module Tr.
+  CoInductive t: Type :=
+  | done
+  | spin
+  .
+End Tr.
+
+Module Flag.
+  Variant t: Type :=
+  | fail
+  | emp
+  | success
+  .
+
+  Definition le: t -> t -> Prop :=
+    fun f0 f1 =>
+      match f0, f1 with
+      | fail, _ => True
+      | _, fail => False
+      | emp, _ => True
+      | _, emp => False
+      | success, _ => True
+      end.
+
+  Global Program Instance le_PreOrder: PreOrder le.
+  Next Obligation.
+    ii. destruct x; ss.
+  Qed.
+  Next Obligation.
+    ii. destruct x, y, z; ss.
+  Qed.
+End Flag.
+
+Section BEHAVES.
+
+  Variable Ident: Type.
+
+  Inductive terminate
+            (R: Type)
+    :
+    forall (itr: itree (eventE Ident) R), Prop :=
+  | terminate_ret
+      r
+    :
+    terminate (Ret r)
+  | terminate_tau
+      itr
+      (DIV: terminate itr)
+    :
+    terminate (Tau itr)
+  | terminate_choose
+      X ktr x
+      (DIV: terminate (ktr x))
+    :
+    terminate (Vis ((Choose Ident) X) ktr)
+  | terminate_success
+      i ktr
+      (DIV: terminate (ktr tt))
+    :
+    terminate (Vis (Success i) ktr)
+  | terminate_fail
+      i ktr
+      (DIV: terminate (ktr tt))
+    :
+    terminate (Vis (Fail i) ktr)
+  .
+
+  Variant _diverge_index
+          (diverge_index: forall (R: Type) (idx: Ident -> nat) (itr: itree (eventE Ident) R), Prop)
+          (R: Type)
+    :
+    forall (idx: Ident -> nat) (itr: itree (eventE Ident) R), Prop :=
+  | diverge_index_tau
+      itr idx0 idx1
+      (DIV: diverge_index _ idx1 itr)
+      (IDX: forall i, idx1 i <= idx0 i)
+    :
+    _diverge_index diverge_index idx0 (Tau itr)
+  | diverge_index_choose
+      X ktr x idx0 idx1
+      (DIV: diverge_index _ idx1 (ktr x))
+      (IDX: forall i, idx1 i <= idx0 i)
+    :
+    _diverge_index diverge_index idx0 (Vis (Choose _ X) ktr)
+  | diverge_index_success
+      i ktr idx0 idx1
+      (DIV: diverge_index _ idx1 (ktr tt))
+      (IDX: forall j (NEQ: j <> i), idx1 j <= idx0 j)
+    :
+    _diverge_index diverge_index idx0 (Vis (Success i) ktr)
+  | diverge_index_fail
+      i ktr idx0 idx1
+      (DIV: diverge_index _ idx1 (ktr tt))
+      (IDX: forall j (NEQ: j <> i), idx1 j <= idx0 j)
+      (DECR: idx1 i < idx0 i)
+    :
+    _diverge_index diverge_index idx0 (Vis (Fail i) ktr)
+  .
+
+  Lemma diverge_index_mon: monotone3 _diverge_index.
+  Proof.
+    ii. inv IN.
+    - econs 1; eauto.
+    - econs 2; eauto.
+    - econs 3; eauto.
+    - econs 4; eauto.
+  Qed.
+
+  Definition diverge_index: forall (R: Type) (idx: Ident -> nat) (itr: itree (eventE _) R), Prop := paco3 _diverge_index bot3.
+
+  Definition diverge (R: Type) (itr: itree (eventE _) R): Prop :=
+    exists idx, diverge_index idx itr.
+
+  Inductive _diverge_flag
+            (diverge_flag: forall (R: Type) (b: bool) (f: Ident -> Flag.t) (itr: itree (eventE _) R), Prop)
+            (R: Type)
+    :
+    forall (b: bool) (f: Ident -> Flag.t) (itr: itree (eventE Ident) R), Prop :=
+  | diverge_flag_tau
+      itr b f0 f1
+      (DIV: _diverge_flag _ true f1 itr)
+      (FLAG: forall i, Flag.le (f1 i) (f0 i))
+    :
+    _diverge_flag diverge_flag b f0 (Tau itr)
+  | diverge_flag_choose
+      X ktr x b f0 f1
+      (DIV: _diverge_flag _ true f1 (ktr x))
+      (FLAG: forall i, Flag.le (f1 i) (f0 i))
+    :
+    _diverge_flag diverge_flag b f0 (Vis (Choose _ X) ktr)
+  | diverge_flag_success
+      i ktr b f0 f1
+      (DIV: _diverge_flag _ true f1 (ktr tt))
+      (FLAG: forall j (NEQ: j <> i), Flag.le (f1 j) (f0 j))
+    :
+    _diverge_flag diverge_flag b f0 (Vis (Success i) ktr)
+  | diverge_flag_fail
+      i ktr b f0 f1
+      (DIV: _diverge_flag _ true f1 (ktr tt))
+      (FLAG: forall j (NEQ: j <> i), Flag.le (f1 j) (f0 j))
+      (FAIL: Flag.le (f0 i) Flag.emp -> Flag.le (f1 i) Flag.emp)
+    :
+    _diverge_flag diverge_flag b f0 (Vis (Fail i) ktr)
+
+  | diverge_flag_progress
+      itr b f
+      (DIV: diverge_flag _ true (fun _ => Flag.emp) itr)
+      (FLAG: forall i, Flag.le Flag.emp (f i))
+    :
+    _diverge_flag diverge_flag b f itr
+  .
+
+  Lemma diverge_flag_mon: monotone4 _diverge_flag.
+  Proof.
+    ii. induction IN.
+    - econs 1; eauto.
+    - econs 2; eauto.
+    - econs 3; eauto.
+    - econs 4; eauto.
+    - econs 5; eauto.
+  Qed.
+
+  Definition diverge_flag: forall (R: Type) (b: bool) (f: Ident -> Flag.t) (itr: itree (eventE _) R), Prop := paco4 _diverge_flag bot4.
+
+  Definition diverge2 (R: Type) (itr: itree (eventE _) R): Prop :=
+    exists b f, diverge_flag b f itr.
+
+  Lemma diverge_diverge2 R (itr: itree (eventE _) R) (DIV: diverge itr)
+    :
+    diverge2 itr.
+  Proof.
+  Admitted.
+
+  Lemma diverge2_diverge R (itr: itree (eventE _) R) (DIV: diverge2 itr)
+    :
+    diverge itr.
+  Proof.
+  Admitted.
+End BEHAVES.
--- src/FairSim.v
+++ src/FairSim.v
@@ -6,203 +6,23 @@ Export ITreeNotations.
 
 Require Import Coq.Classes.RelationClasses.
 
+From Fairness Require Import ITreeLib.
+From Fairness Require Import FairBeh.
+
 Set Implicit Arguments.
 
-Variable Ident: Type.
-
-Variant eventE: Type -> Type :=
-| Choose (X: Type): eventE X
-| Success (i: Ident): eventE unit
-| Fail (i: Ident): eventE unit
-.
-
-Module Tr.
-  CoInductive t: Type :=
-  | done
-  | spin
-  .
-End Tr.
-
-Module Flag.
-  Variant t: Type :=
-  | fail
-  | emp
-  | success
-  .
-
-  Definition le: t -> t -> Prop :=
-    fun f0 f1 =>
-      match f0, f1 with
-      | fail, _ => True
-      | _, fail => False
-      | emp, _ => True
-      | _, emp => False
-      | success, _ => True
-      end.
-
-  Global Program Instance le_PreOrder: PreOrder le.
-  Next Obligation.
-    ii. destruct x; ss.
-  Qed.
-  Next Obligation.
-    ii. destruct x, y, z; ss.
-  Qed.
-End Flag.
-
-Section BEHAVES.
-  Inductive terminate
-            (R: Type)
-    :
-    forall (itr: itree eventE R), Prop :=
-  | terminate_ret
-      r
-    :
-    terminate (Ret r)
-  | terminate_tau
-      itr
-      (DIV: terminate itr)
-    :
-    terminate (Tau itr)
-  | terminate_choose
-      X ktr x
-      (DIV: terminate (ktr x))
-    :
-    terminate (Vis (Choose X) ktr)
-  | terminate_success
-      i ktr
-      (DIV: terminate (ktr tt))
-    :
-    terminate (Vis (Success i) ktr)
-  | terminate_fail
-      i ktr
-      (DIV: terminate (ktr tt))
-    :
-    terminate (Vis (Fail i) ktr)
-  .
-
-  Variant _diverge_index
-          (diverge_index: forall (R: Type) (idx: Ident -> nat) (itr: itree eventE R), Prop)
-          (R: Type)
-    :
-    forall (idx: Ident -> nat) (itr: itree eventE R), Prop :=
-  | diverge_index_tau
-      itr idx0 idx1
-      (DIV: diverge_index _ idx1 itr)
-      (IDX: forall i, idx1 i <= idx0 i)
-    :
-    _diverge_index diverge_index idx0 (Tau itr)
-  | diverge_index_choose
-      X ktr x idx0 idx1
-      (DIV: diverge_index _ idx1 (ktr x))
-      (IDX: forall i, idx1 i <= idx0 i)
-    :
-    _diverge_index diverge_index idx0 (Vis (Choose X) ktr)
-  | diverge_index_success
-      i ktr idx0 idx1
-      (DIV: diverge_index _ idx1 (ktr tt))
-      (IDX: forall j (NEQ: j <> i), idx1 j <= idx0 j)
-    :
-    _diverge_index diverge_index idx0 (Vis (Success i) ktr)
-  | diverge_index_fail
-      i ktr idx0 idx1
-      (DIV: diverge_index _ idx1 (ktr tt))
-      (IDX: forall j (NEQ: j <> i), idx1 j <= idx0 j)
-      (DECR: idx1 i < idx0 i)
-    :
-    _diverge_index diverge_index idx0 (Vis (Fail i) ktr)
-  .
-
-  Lemma diverge_index_mon: monotone3 _diverge_index.
-  Proof.
-    ii. inv IN.
-    - econs 1; eauto.
-    - econs 2; eauto.
-    - econs 3; eauto.
-    - econs 4; eauto.
-  Qed.
-
-  Definition diverge_index: forall (R: Type) (idx: Ident -> nat) (itr: itree eventE R), Prop := paco3 _diverge_index bot3.
-
-  Definition diverge (R: Type) (itr: itree eventE R): Prop :=
-    exists idx, diverge_index idx itr.
-
-  Inductive _diverge_flag
-            (diverge_flag: forall (R: Type) (b: bool) (f: Ident -> Flag.t) (itr: itree eventE R), Prop)
-            (R: Type)
-    :
-    forall (b: bool) (f: Ident -> Flag.t) (itr: itree eventE R), Prop :=
-  | diverge_flag_tau
-      itr b f0 f1
-      (DIV: _diverge_flag _ true f1 itr)
-      (FLAG: forall i, Flag.le (f1 i) (f0 i))
-    :
-    _diverge_flag diverge_flag b f0 (Tau itr)
-  | diverge_flag_choose
-      X ktr x b f0 f1
-      (DIV: _diverge_flag _ true f1 (ktr x))
-      (FLAG: forall i, Flag.le (f1 i) (f0 i))
-    :
-    _diverge_flag diverge_flag b f0 (Vis (Choose X) ktr)
-  | diverge_flag_success
-      i ktr b f0 f1
-      (DIV: _diverge_flag _ true f1 (ktr tt))
-      (FLAG: forall j (NEQ: j <> i), Flag.le (f1 j) (f0 j))
-    :
-    _diverge_flag diverge_flag b f0 (Vis (Success i) ktr)
-  | diverge_flag_fail
-      i ktr b f0 f1
-      (DIV: _diverge_flag _ true f1 (ktr tt))
-      (FLAG: forall j (NEQ: j <> i), Flag.le (f1 j) (f0 j))
-      (FAIL: Flag.le (f0 i) Flag.emp -> Flag.le (f1 i) Flag.emp)
-    :
-    _diverge_flag diverge_flag b f0 (Vis (Fail i) ktr)
-
-  | diverge_flag_progress
-      itr b f
-      (DIV: diverge_flag _ true (fun _ => Flag.emp) itr)
-      (FLAG: forall i, Flag.le Flag.emp (f i))
-    :
-    _diverge_flag diverge_flag b f itr
-  .
-
-  Lemma diverge_flag_mon: monotone4 _diverge_flag.
-  Proof.
-    ii. induction IN.
-    - econs 1; eauto.
-    - econs 2; eauto.
-    - econs 3; eauto.
-    - econs 4; eauto.
-    - econs 5; eauto.
-  Qed.
-
-  Definition diverge_flag: forall (R: Type) (b: bool) (f: Ident -> Flag.t) (itr: itree eventE R), Prop := paco4 _diverge_flag bot4.
-
-  Definition diverge2 (R: Type) (itr: itree eventE R): Prop :=
-    exists b f, diverge_flag b f itr.
-
-  Lemma diverge_diverge2 R (itr: itree eventE R) (DIV: diverge itr)
-    :
-    diverge2 itr.
-  Proof.
-  Admitted.
-
-  Lemma diverge2_diverge R (itr: itree eventE R) (DIV: diverge2 itr)
-    :
-    diverge itr.
-  Proof.
-  Admitted.
-End BEHAVES.
-
-
-
 Section SIM.
+
+  Variable Ident: Type.
 
   Definition fmap := Ident -> Flag.t.
   Definition fmap_emp : fmap := fun _ => Flag.emp.
 
   Inductive _sim
-            (sim: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> fmap  -> bool -> fmap -> Ident -> (itree eventE R0) -> (itree eventE R1) -> Prop)
-            {R0 R1} (RR: R0 -> R1 -> Prop) (p_src: bool) (m_src: fmap) (p_tgt: bool) (m_tgt: fmap) (idx: Ident) : (itree eventE R0) -> (itree eventE R1) -> Prop :=
+            (sim: forall R0 R1 (RR: R0 -> R1 -> Prop),
+                bool -> fmap  -> bool -> fmap -> Ident -> (itree (eventE Ident) R0) -> (itree (eventE Ident) R1) -> Prop)
+            {R0 R1} (RR: R0 -> R1 -> Prop) (p_src: bool) (m_src: fmap) (p_tgt: bool) (m_tgt: fmap) (idx: Ident) :
+    (itree (eventE _) R0) -> (itree (eventE _) R1) -> Prop :=
   | sim_ret
       r_src r_tgt
       (SIM: RR r_src r_tgt)
@@ -223,12 +43,12 @@ Section SIM.
       X ktr_src0 itr_tgt0
       (SIM: exists x, _sim sim RR true m_src p_tgt m_tgt idx (ktr_src0 x) itr_tgt0)
     :
-    _sim sim RR p_src m_src p_tgt m_tgt idx (trigger (Choose X) >>= ktr_src0) itr_tgt0
+    _sim sim RR p_src m_src p_tgt m_tgt idx (trigger (Choose _ X) >>= ktr_src0) itr_tgt0
   | sim_chooseR
       X itr_src0 ktr_tgt0
       (SIM: forall x, _sim sim RR p_src m_src true m_tgt idx itr_src0 (ktr_tgt0 x))
     :
-    _sim sim RR p_src m_src p_tgt m_tgt idx itr_src0 (trigger (Choose X) >>= ktr_tgt0)
+    _sim sim RR p_src m_src p_tgt m_tgt idx itr_src0 (trigger (Choose _ X) >>= ktr_tgt0)
 
   | sim_failL
       id m_src0 ktr_src0 itr_tgt0
@@ -280,9 +100,9 @@ Section SIM.
     _sim sim RR p_src m_src p_tgt m_tgt idx itr_src0 itr_tgt0
   .
 
-  Lemma _sim_ind2 (r: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> fmap  -> bool -> fmap -> Ident -> (itree eventE R0) -> (itree eventE R1) -> Prop)
+  Lemma _sim_ind2 (r: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> fmap  -> bool -> fmap -> Ident -> (itree (eventE Ident) R0) -> (itree (eventE Ident) R1) -> Prop)
         R0 R1 (RR: R0 -> R1 -> Prop)
-        (P: bool -> fmap  -> bool -> fmap -> Ident -> (itree eventE R0) -> (itree eventE R1) -> Prop)
+        (P: bool -> fmap  -> bool -> fmap -> Ident -> (itree (eventE Ident) R0) -> (itree (eventE Ident) R1) -> Prop)
         (RET: forall
             p_src m_src p_tgt m_tgt idx
             r_src r_tgt
@@ -303,12 +123,12 @@ Section SIM.
             p_src m_src p_tgt m_tgt idx X ktr_src0 itr_tgt0
             (SIM: exists x, <<SIM: _sim r RR true m_src p_tgt m_tgt idx (ktr_src0 x) itr_tgt0>> /\
                                 <<IH: P true m_src p_tgt m_tgt idx (ktr_src0 x) itr_tgt0>>),
-            P p_src m_src p_tgt m_tgt idx (ITree.bind (trigger (Choose X)) ktr_src0) itr_tgt0)
+            P p_src m_src p_tgt m_tgt idx (ITree.bind (trigger (Choose _ X)) ktr_src0) itr_tgt0)
         (CHOOSER: forall
             p_src m_src p_tgt m_tgt idx X itr_src0 ktr_tgt0
             (SIM: forall x, <<SIM: _sim r RR p_src m_src true m_tgt idx itr_src0 (ktr_tgt0 x)>> /\
                                 <<IH: P p_src m_src true m_tgt idx itr_src0 (ktr_tgt0 x)>>),
-            P p_src m_src p_tgt m_tgt idx itr_src0 (ITree.bind (trigger (Choose X)) ktr_tgt0))
+            P p_src m_src p_tgt m_tgt idx itr_src0 (ITree.bind (trigger (Choose _ X)) ktr_tgt0))
         (FAILL: forall
             p_src m_src p_tgt m_tgt idx ktr_src0 itr_tgt0
             id m_src0
@@ -377,7 +197,7 @@ Section SIM.
     { eapply PROGRESSW; eauto. }
   Qed.
 
-  Definition sim: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> fmap  -> bool -> fmap -> Ident -> (itree eventE R0) -> (itree eventE R1) -> Prop := paco10 _sim bot10.
+  Definition sim: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> fmap  -> bool -> fmap -> Ident -> (itree (eventE _) R0) -> (itree (eventE _) R1) -> Prop := paco10 _sim bot10.
 
   Lemma sim_mon: monotone10 _sim.
   Proof.
@@ -403,8 +223,10 @@ Section SIM.
   Definition imap := Ident -> nat.
 
   Inductive _isim
-            (isim: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> imap  -> bool -> imap -> (itree eventE R0) -> (itree eventE R1) -> Prop)
-            {R0 R1} (RR: R0 -> R1 -> Prop) (p_src: bool) (m_src: imap) (p_tgt: bool) (m_tgt: imap): (itree eventE R0) -> (itree eventE R1) -> Prop :=
+            (isim: forall R0 R1 (RR: R0 -> R1 -> Prop),
+                bool -> imap  -> bool -> imap -> (itree (eventE Ident) R0) -> (itree (eventE Ident) R1) -> Prop)
+            {R0 R1} (RR: R0 -> R1 -> Prop) (p_src: bool) (m_src: imap) (p_tgt: bool) (m_tgt: imap):
+    (itree (eventE Ident) R0) -> (itree (eventE Ident) R1) -> Prop :=
   | isim_ret
       r_src r_tgt
       (SIM: RR r_src r_tgt)
@@ -424,12 +246,12 @@ Section SIM.
       X ktr_src0 itr_tgt0
       (SIM: exists x, _isim isim RR true m_src p_tgt m_tgt (ktr_src0 x) itr_tgt0)
     :
-    _isim isim RR p_src m_src p_tgt m_tgt (trigger (Choose X) >>= ktr_src0) itr_tgt0
+    _isim isim RR p_src m_src p_tgt m_tgt (trigger (Choose _ X) >>= ktr_src0) itr_tgt0
   | isim_chooseR
       X itr_src0 ktr_tgt0
       (SIM: forall x, _isim isim RR p_src m_src true m_tgt itr_src0 (ktr_tgt0 x))
     :
-    _isim isim RR p_src m_src p_tgt m_tgt itr_src0 (trigger (Choose X) >>= ktr_tgt0)
+    _isim isim RR p_src m_src p_tgt m_tgt itr_src0 (trigger (Choose _ X) >>= ktr_tgt0)
 
   | isim_failL
       id m_src0 ktr_src0 itr_tgt0
@@ -468,9 +290,9 @@ Section SIM.
     _isim isim RR p_src m_src p_tgt m_tgt itr_src0 itr_tgt0
   .
 
-  Lemma _isim_ind2 (r: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> imap  -> bool -> imap -> (itree eventE R0) -> (itree eventE R1) -> Prop)
+  Lemma _isim_ind2 (r: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> imap  -> bool -> imap -> (itree (eventE Ident) R0) -> (itree (eventE Ident) R1) -> Prop)
         R0 R1 (RR: R0 -> R1 -> Prop)
-        (P: bool -> imap  -> bool -> imap -> (itree eventE R0) -> (itree eventE R1) -> Prop)
+        (P: bool -> imap  -> bool -> imap -> (itree (eventE Ident) R0) -> (itree (eventE Ident) R1) -> Prop)
         (RET: forall
             p_src m_src p_tgt m_tgt
             r_src r_tgt
@@ -490,12 +312,12 @@ Section SIM.
             p_src m_src p_tgt m_tgt X ktr_src0 itr_tgt0
             (SIM: exists x, <<SIM: _isim r RR true m_src p_tgt m_tgt (ktr_src0 x) itr_tgt0>> /\
                                 <<IH: P true m_src p_tgt m_tgt (ktr_src0 x) itr_tgt0>>),
-            P p_src m_src p_tgt m_tgt (ITree.bind (trigger (Choose X)) ktr_src0) itr_tgt0)
+            P p_src m_src p_tgt m_tgt (ITree.bind (trigger (Choose _ X)) ktr_src0) itr_tgt0)
         (CHOOSER: forall
             p_src m_src p_tgt m_tgt X itr_src0 ktr_tgt0
             (SIM: forall x, <<SIM: _isim r RR p_src m_src true m_tgt itr_src0 (ktr_tgt0 x)>> /\
                                 <<IH: P p_src m_src true m_tgt itr_src0 (ktr_tgt0 x)>>),
-            P p_src m_src p_tgt m_tgt itr_src0 (ITree.bind (trigger (Choose X)) ktr_tgt0))
+            P p_src m_src p_tgt m_tgt itr_src0 (ITree.bind (trigger (Choose _ X)) ktr_tgt0))
         (FAILL: forall
             p_src m_src p_tgt m_tgt ktr_src0 itr_tgt0
             id m_src0
@@ -551,7 +373,7 @@ Section SIM.
     { eapply PROGRESSS; eauto. }
   Qed.
 
-  Definition isim: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> imap  -> bool -> imap -> (itree eventE R0) -> (itree eventE R1) -> Prop := paco9 _isim bot9.
+  Definition isim: forall R0 R1 (RR: R0 -> R1 -> Prop), bool -> imap  -> bool -> imap -> (itree (eventE _) R0) -> (itree (eventE _) R1) -> Prop := paco9 _isim bot9.
 
   Lemma isim_mon: monotone9 _isim.
   Proof.
@@ -572,3 +394,78 @@ Section SIM.
   Hint Resolve cpn9_wcompat: paco.
 
 End SIM.
+
+
+
+Section EX.
+
+  Definition Ident := nat.
+
+  (* Definition uunit := prod unit unit. *)
+
+  Definition while_itree (step: unit -> itree (eventE Ident) (unit + unit)) : itree (eventE Ident) unit :=
+    ITree.iter step tt.
+
+  Lemma unfold_iter_eq A E B
+     : forall (f : A -> itree E (A + B)) (x : A),
+       ITree.iter f x = lr <- f x;; match lr with
+                                    | inl l => Tau (ITree.iter f l)
+                                    | inr r0 => Ret r0
+                                   end.
+  Proof.
+    i. eapply bisim_is_eq. eapply unfold_iter.
+  Qed.
+
+  Require Import Program.
+  
+  Lemma unfold_while_itree
+        itr
+    :
+    while_itree (fun u => ITree.bind itr (fun r => Ret (inl r))) =
+      (* r <- itr;; *)
+      (* x <- Ret (inl r);; *)
+      (* match x with *)
+      (* | inl l => *)
+      (*     Tau *)
+      (*       (ITree.iter (fun _ : unit => r0 <- itr;; Ret (inl r0)) *)
+      (*                   l) *)
+      (* | inr r0 => Ret r0 *)
+      (* end. *)
+      itr;; tau;; (while_itree (fun u => ITree.bind itr (fun r => Ret (inl r)))).
+  Proof.
+    unfold while_itree. rewrite unfold_iter_eq at 1. ired.
+    f_equal. extensionality u. destruct u. ired. auto.
+  Qed.
+    (* unfold while_itree. eapply bisim_is_eq. revert1. ginit. gcofix CIH. i. *)
+    (* rewrite unfold_iter_eq. ired. guclo eqit_clo_bind. econs. reflexivity. *)
+    (* i; subst. ired. gstep. econs. gfinal. left. *)
+    (* specialize (CIH itr). destruct u2. *)
+    (* match goal with *)
+    (* | _: r _ ?x |- r _ ?y => replace y with x *)
+    (* end; eauto. *)
+    (* rewrite unfold_iter_eq. ired. *)
+    (* f_equal. extensionality a. destruct a. ired. f_equal. f_equal. *)
+    (* rewrite unfold_iter_eq. ired. auto. *)
+
+  Definition RR := @eq nat.
+
+  Definition src1 : itree (eventE Ident) nat := Ret 0.
+  Definition tgt1 : itree (eventE Ident) nat :=
+    while_itree (fun u => ITree.bind (trigger (Fail 0)) (fun r => Ret (inl r)));; Ret 0.
+
+  Goal sim RR false (@fmap_emp _) false (@fmap_emp _) 0 src1 tgt1.
+  Proof.
+    unfold src1, tgt1.
+    pcofix CIH. pfold. rewrite unfold_while_itree. ired.
+    econs 7.
+    { i. instantiate (1:= fun n => if PeanoNat.Nat.eq_dec 0 n then Flag.fail else Flag.emp).
+      ss. des_ifs.
+    }
+    { i. ss. }
+    econs. econs 11; auto.
+    3:{ right. eauto. }
+    { i. des_ifs. }
+    ss.
+  Qed.
+
+End EX.
