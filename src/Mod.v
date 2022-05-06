566b953a2b53b6c08df13a4b38d7979396073ac7
minkiminki <minki.cho@sf.snu.ac.kr>
2022-05-06 13:40:37 +0900

    Add Mod add operation

--- src/Mod.v
+++ src/Mod.v
@@ -1,5 +1,5 @@
 From sflib Require Import sflib.
-From ITree Require Export ITree.
+From ITree Require Export ITree Subevent.
 From Paco Require Import paco.
 
 From ITree Require Export
@@ -11,24 +11,79 @@ From ITree Require Export
      EqAxiom
 .
 
+From ExtLib Require Export
+     (* Data.String *)
+     (* Structures.Monad *)
+     (* Structures.Traversable *)
+     (* Structures.Foldable *)
+     (* Structures.Reducible *)
+     (* OptionMonad *)
+     Functor FunctorLaws
+     Structures.Maps
+     (* Data.List *)
+.
+
+Export SumNotations.
+Export Monads.
+Export CatNotations.
+Open Scope cat_scope.
+Open Scope monad_scope.
+Open Scope itree_scope.
 Export ITreeNotations.
+Export CatNotations.
 
 Require Import Coq.Classes.RelationClasses.
 
 Set Implicit Arguments.
+
+Definition update_fst {A B}: A * B -> A -> A * B :=
+  fun '(_, b) a => (a, b).
+
+Definition update_snd {A B}: A * B -> B -> A * B :=
+  fun '(a, _) b => (a, b).
 
 Variant cE (State: Type): Type -> Type :=
 | Yield (st: State): cE State State
 .
 
 Variable BehE: Type -> Type.
+Variable UB: BehE void.
+
+Section LENS.
+  Variable S: Type.
+  Variable V: Type.
+  Variable E: Type -> Type.
+
+  Variable get: S -> V.
+  Variable put: S -> V -> S.
+
+  Definition embed_itree:
+    forall (s: S) R (itr: itree (E +' cE V) R),
+      itree (E +' cE S) (R * S).
+    cofix embed_itree.
+    intros s R itr.
+    destruct (observe itr) as [r|itr0|? [e|[]] ktr].
+    { exact (Ret (r, s)). }
+    { exact (Tau (embed_itree s _ itr0)). }
+    { exact (Vis (inl1 e) (fun x => embed_itree s _ (ktr x))). }
+    { exact (Vis (inr1 (Yield (put s st))) (fun s => embed_itree s _ (ktr (get s)))). }
+    Show Proof.
+  Defined.
+
+  Definition embed_fun A B (ktr: ktree (E +' cE V) (A * V) (B * V)):
+    ktree (E +' cE S) (A * S) (B * S) :=
+    fun '(a, s) =>
+      '(b, _, s) <- embed_itree s (ktr (a, get s));; Ret (b, s).
+End LENS.
 
 Variable fname: Type.
 Variable fname_eq_dec: forall (f0 f1: fname), {f0 = f1} + {f0 <> f1}.
+Variable fname_parse: fname -> option (bool * fname).
+
 Variable Val: Type.
 
 Variant callE (State: Type): Type -> Type :=
-| Call (arg: Val) (st: State): callE State (Val * State)
+| Call (fn: fname) (arg: Val) (st: State): callE State (Val * State)
 .
 
 Module Mod.
@@ -48,39 +103,34 @@ Module Mod.
     Definition add_st_init: state0 -> state1 -> add_state :=
       fun st_init0 st_init1 => (st_init0, st_init1).
 
-    Definition add_cE_left (st1: state1):
-      forall X, cE state0 X -> cE add_state (X * state1) :=
-      fun X e =>
-        match e with
-        | Yield st0 => Yield (st0, st1)
-        end.
+    Definition add_fun_left
+               (ktr: ktree (BehE +' cE state0) (Val * state0) (Val * state0)):
+      ktree (BehE +' cE add_state) (Val * add_state) (Val * add_state) :=
+      embed_fun fst update_fst ktr.
 
-    CoFixpoint add_itree_left:
-      forall (st1: state1) R (itr: itree (BehE +' cE state0) R),
-        itree (BehE +' cE add_state) (R * state1) :=
-      fun st1 R itr =>
-        match observe itr with
-        | RetF r => Ret (r, st1)
-        | TauF itr => Tau (add_itree_left st1 itr)
-        | VisF (inl1 e) ktr =>
-            Vis (inl1 e) (fun x => add_itree_left st1 (ktr x))
-        | VisF (inr1 e) ktr =>
-            Vis (inr1 (add_cE_left st1 e)) (fun '(x, st1) => add_itree_left st1 (ktr x))
-        end.
+    Definition add_fun_right
+               (ktr: ktree (BehE +' cE state1) (Val * state1) (Val * state1)):
+      ktree (BehE +' cE add_state) (Val * add_state) (Val * add_state) :=
+      embed_fun snd update_snd ktr.
 
-    Lemma add_itree_left_ret st1 R (r: R)
+    Definition add_funs
+               (funs0: fname -> ktree (BehE +' cE state0) (Val * state0) (Val * state0))
+               (funs1: fname -> ktree (BehE +' cE state1) (Val * state1) (Val * state1))
       :
-      add_itree_left st1 (Ret r) = Ret (r, st1).
-    Admitted.
-
-    Lemma add_itree_left_tau st1 R (itr: itree (BehE +' cE state0) R)
-      :
-      add_itree_left st1 (Tau itr) = Tau (add_itree_left st1 itr).
-    Admitted.
+      fname -> ktree (BehE +' cE add_state) (Val * add_state) (Val * add_state) :=
+      fun fn =>
+        match fname_parse fn with
+        | Some (true, fn) => add_fun_left (funs0 fn)
+        | Some (false, fn) => add_fun_right (funs1 fn)
+        | None => fun _ => trigger UB >>= Empty_set_rect _
+        end.
   End ADD.
 
-  Definition add: t -> t -> t.
-  Admitted.
+  Definition add (md0 md1: t): t :=
+    @mk
+      (add_state md0.(state) md1.(state))
+      (add_st_init md0.(st_init) md1.(st_init))
+      (add_funs md0.(funs) md1.(funs)).
 End Mod.
 
 Module ModSim.
@@ -116,5 +166,11 @@ Module OMod.
     Definition link_state: Type := omd.(state) * md.(Mod.state).
 
     Definition link_st_init: link_state := (omd.(st_init), md.(Mod.st_init)).
+
+    Definition link_itree:
+      forall (s: link_state) R,
+        itree (BehE +' callE omd.(state) +' cE omd.(state)) R -> itree (BehE +' callE link_state +' cE link_state) (link_state * R).
+    Proof.
+    Admitted.
   End LINK.
 End OMod.
