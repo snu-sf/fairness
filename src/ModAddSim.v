87c0ad9e9ea9ebf01da9520563b4db3a854d8433
damhiya <damhiya@gmail.com>
2022-07-18 16:08:00 +0900

    complete proof

--- src/ModAddSim.v
+++ src/ModAddSim.v
@@ -136,9 +136,94 @@ Section ADD_MODSIM.
                 destruct st_src0, st_tgt0. des. ss. subst.
                 eapply CIH; ss.
           }
-      + unfold embed_l, embed_r. ii. admit.
+      + ii. unfold embed_l, embed_r.
+        remember (k args) as itr; clear fn k args Heqitr THS.
+        revert fs ft itr ths0 im_src0 im_tgt0 st_src0 st_tgt0 w0 tid INV.
+        ginit. gcofix CIH. i.
+
+        destruct_itree itr.
+        * rewrite 2 embed_state_ret.
+          rewrite 2 map_event_ret.
+          gstep.
+          eapply pind5.pind5_fold.
+          econs. inv INV. des. ss. esplits; ss.
+        * rewrite 2 embed_state_tau.
+          rewrite 2 map_event_tau.
+          gstep.
+          eapply pind5.pind5_fold. eapply lsim_tauL. esplit; ss.
+          eapply pind5.pind5_fold. eapply lsim_tauR. esplit; ss.
+          eapply pind5.pind5_fold. eapply lsim_progress.
+          gfinal. left. eapply CIH; ss.
+        * { destruct e as [[|] | ].
+            - rewrite 2 embed_state_vis.
+              rewrite 2 map_event_vis.
+              rewrite <- 2 bind_trigger.
+              gstep. destruct e; ss.
+              + eapply pind5.pind5_fold. eapply lsim_chooseR. i. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_chooseL. exists x. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_progress.
+                gfinal. left. eapply CIH; ss.
+              + eapply pind5.pind5_fold. eapply lsim_fairR. i. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_fairL. exists (m_conv im_tgt1). esplit.
+                { unfold m_eq in INV; des. ii.
+                  destruct i as [|[]]; ss.
+                  - specialize (FAIR (inl i)). rewrite INV1. ss.
+                  - specialize (FAIR (inr (inr i))). rewrite INV2. ss.
+                  - specialize (FAIR (inr (inl i))). rewrite INV3. ss.
+                }
+                esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_progress.
+                gfinal. left. des. eapply CIH; ss. 
+              + eapply pind5.pind5_fold. eapply lsim_observe. i.
+                gfinal. left. eapply CIH; ss.
+              + eapply pind5.pind5_fold. eapply lsim_UB.
+            - rewrite 2 embed_state_vis.
+              rewrite 2 map_event_vis. ss.
+              rewrite <- 2 bind_trigger.
+              gstep. destruct c.
+              + eapply pind5.pind5_fold. eapply lsim_sync; ss. i. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_yieldL. exists (m_conv im_tgt2). esplit.
+                { unfold m_eq in INV0; des. ii.
+                  destruct i as [|[]]; ss.
+                  - specialize (TGT (inl i)). rewrite INV2. ss.
+                  - specialize (TGT (inr (inr i))). rewrite INV3. ss.
+                  - specialize (TGT (inr (inl i))). rewrite INV4. ss.
+                }
+                esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_progress.
+                gfinal. left. des. eapply CIH; ss.
+              + eapply pind5.pind5_fold. eapply lsim_tidR. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_tidL. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_progress.
+                gfinal. left. eapply CIH; ss.
+            - destruct s.
+              + rewrite 2 embed_state_put. ss.
+                rewrite 2 map_event_vis. ss.
+                rewrite <- 2 bind_trigger.
+                gstep.
+                eapply pind5.pind5_fold. eapply lsim_getL. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_getR. esplit; ss.
+                rewrite 2 map_event_vis. ss.
+                rewrite <- 2 bind_trigger.
+                eapply pind5.pind5_fold. eapply lsim_putL. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_putR. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_progress.
+                gfinal. left. eapply CIH; ss.
+                des. destruct st_src0, st_tgt0. ss.
+              + rewrite 2 embed_state_get. ss.
+                rewrite 2 map_event_vis. ss.
+                rewrite <- 2 bind_trigger.
+                gstep.
+                eapply pind5.pind5_fold. eapply lsim_getL. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_getR. esplit; ss.
+                eapply pind5.pind5_fold. eapply lsim_progress.
+                gfinal. left.
+                destruct st_src0, st_tgt0. des. ss. subst.
+                eapply CIH; ss.
+          }
       + ss.
-  Admitted.
+        Unshelve. all: eauto.
+  Qed.
 
   Lemma ModAdd_right_cong M1 M2_src M2_tgt :
     ModSim.mod_sim M2_src M2_tgt ->
