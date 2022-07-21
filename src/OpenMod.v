c369c98488455ff7d096aac64f4dff5910c503c8
ldj <dongjae.lee@sf.snu.ac.kr>
2022-07-21 15:06:12 +0900

    WIP

--- src/OpenMod.v
+++ src/OpenMod.v
@@ -1,4 +1,5 @@
 From sflib Require Import sflib.
+From ITree Require Export ITree.
 From Paco Require Import paco.
 
 Require Export Coq.Strings.String.
@@ -31,11 +32,29 @@ Module OMod.
     Definition closed_ident: ID := id_sum omd.(ident) md.(Mod.ident).
     Definition closed_st_init: closed_state := (omd.(st_init), md.(Mod.st_init)).
 
+    (* Definition handle_callE *)
+    (*            {T} *)
+    (*            (e: ((((@eventE omd.(ident)) +' cE) +' (sE omd.(state))) +' callE) T) *)
+    (*   : *)
+
     Definition closed_itree {A R}:
       (ktree ((((@eventE omd.(ident)) +' cE) +' (sE omd.(state))) +' callE) (list A) R) ->
-      (ktree ((((@eventE closed_ident) +' cE) +' (sE closed_state)) +' callE) (list A) R).
+      (ktree ((((@eventE closed_ident) +' cE) +' (sE closed_state))) (list A) R).
     Proof.
-      
+      (*ub for undefined fn call*)
+      ii. specialize (X X0); clear X0 A. revert X.
+      eapply ITree.iter. intros itr. destruct (observe itr).
+      - exact (Ret (inr r)).
+      - exact (Ret (inl t0)).
+      - destruct e as [[[eE|cE]|stE]|caE].
+        + exact (Vis ((embed_event_l eE|)|)%sum (fun x => Ret (inl (k x)))).
+        + exact (Vis ((|cE)|)%sum (fun x => Ret (inl (k x)))).
+        + eapply embed_state. instantiate (1:=omd.(state)). exact fst. exact update_fst.
+          exact (Vis (|stE)%sum (fun x => Ret (inl (k x)))).
+        + destruct caE.
+          destruct (md.(Mod.funs) fn) eqn:FUN.
+          * 
+          
       
 
     Definition closed_itree:
