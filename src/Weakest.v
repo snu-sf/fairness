1c1e76b18887c2a13a3ed08dca18faf83567cbb9
Minki Cho <minki.cho@sf.snu.ac.kr>
2022-08-18 16:27:13 +0900

    State separation logic style lemmas

--- src/Weakest.v
+++ src/Weakest.v
@@ -43,20 +43,64 @@ Section SIM.
   Variable I: shared_rel.
 
   Program Definition isim (tid: thread_id) (R_src R_tgt: Type)
-          (RR: R_src -> R_tgt -> shared_rel):
+          (Q: R_src -> R_tgt -> shared_rel):
     itree srcE R_src -> itree tgtE R_tgt -> shared_rel :=
     fun itr_src itr_tgt ths im_src im_tgt st_src st_tgt =>
       iProp_intro
         (fun r =>
            forall r_ctx (WF: URA.wf (r ⋅ r_ctx)),
-             lsim (lift I) tid (liftRR RR) false false r_ctx itr_src itr_tgt (ths, im_src, im_tgt, st_src, st_tgt)) _.
+             lsim (lift I) tid (liftRR Q) false false r_ctx itr_src itr_tgt (ths, im_src, im_tgt, st_src, st_tgt)) _.
   Next Obligation.
   Proof.
     ii. ss. eapply H.
     eapply URA.wf_extends; eauto. eapply URA.extends_add; eauto.
   Qed.
 
-  Lemma isim
+  Lemma isim_mono tid R_src R_tgt
+        (Q0 Q1: R_src -> R_tgt -> shared_rel)
+        (MONO: forall r_src r_tgt ths im_src im_tgt st_src st_tgt,
+            bi_entails
+              (Q0 r_src r_tgt ths im_src im_tgt st_src st_tgt)
+              (Q1 r_src r_tgt ths im_src im_tgt st_src st_tgt))
+        itr_src itr_tgt ths im_src im_tgt st_src st_tgt
+    :
+    bi_entails
+      (isim tid Q0 itr_src itr_tgt ths im_src im_tgt st_src st_tgt)
+      (isim tid Q1 itr_src itr_tgt ths im_src im_tgt st_src st_tgt)
+  .
+  Admitted.
 
+  Lemma isim_upd tid R_src R_tgt
+        (Q: R_src -> R_tgt -> shared_rel)
+        itr_src itr_tgt ths im_src im_tgt st_src st_tgt
+    :
+    bi_entails
+      (#=> (isim tid Q itr_src itr_tgt ths im_src im_tgt st_src st_tgt))
+      (isim tid Q itr_src itr_tgt ths im_src im_tgt st_src st_tgt)
+  .
+  Admitted.
 
+  Lemma isim_wand tid R_src R_tgt
+        (Q0 Q1: R_src -> R_tgt -> shared_rel)
+        itr_src itr_tgt ths im_src im_tgt st_src st_tgt
+    :
+    bi_entails
+      ((∀ r_src r_tgt ths im_src im_tgt st_src st_tgt,
+           ((Q0 r_src r_tgt ths im_src im_tgt st_src st_tgt) -∗ (Q1 r_src r_tgt ths im_src im_tgt st_src st_tgt))) ** (isim tid Q0 itr_src itr_tgt ths im_src im_tgt st_src st_tgt))
+      (isim tid Q1 itr_src itr_tgt ths im_src im_tgt st_src st_tgt)
+  .
+  Admitted.
+
+  Lemma isim_bind tid R_src R_tgt S_src S_tgt
+        (Q: R_src -> R_tgt -> shared_rel)
+        (itr_src: itree srcE S_src) (itr_tgt: itree tgtE S_tgt)
+        ktr_src ktr_tgt
+        ths im_src im_tgt st_src st_tgt
+    :
+    bi_entails
+      (isim tid (fun s_src s_tgt ths im_src im_tgt st_src st_tgt =>
+                   isim tid Q (ktr_src s_src) (ktr_tgt s_tgt) ths im_src im_tgt st_src st_tgt) itr_src itr_tgt ths im_src im_tgt st_src st_tgt)
+      (isim tid Q (itr_src >>= ktr_src) (itr_tgt >>= ktr_tgt) ths im_src im_tgt st_src st_tgt)
+  .
+  Admitted.
 End SIM.
