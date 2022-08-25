f3d67baa636232c0b282a8fb0e0a20f5424504b0
ldj <dongjae.lee@sf.snu.ac.kr>
2022-08-25 18:54:42 +0900

    WIP

--- src/ModAdequacy.v
+++ src/ModAdequacy.v
@@ -553,78 +553,26 @@ Section ADEQ.
     let pre_threads := List.map (fun '(fn, args) => fn2th m fn args) p in
     NatMapP.of_list (numbering pre_threads).
 
+  Theorem modsim_adequacy
+          m_src m_tgt
+          (MSIM: ModSim.ModSim.mod_sim m_src m_tgt)
+    :
+    forall tid (p: program),
+      Adequacy.improves (interp_all m_src.(Mod.st_init) (prog2ths m_src p) tid)
+                        (interp_all m_tgt.(Mod.st_init) (prog2ths m_tgt p) tid).
+  Proof.
+    apply modsim_implies_yord_mod in MSIM.
+    apply yord_implies_stid_mod in MSIM.
+    apply stid_implies_nosync_mod in MSIM.
+    apply nosync_implies_stutter_mod in MSIM.
+    inv MSIM. i.
+    eapply Adequacy.adequacy. eapply wf_tgt_inhabited. eapply wf_tgt_open.
+    instantiate (1:=wf_src).
+    eapply ModSimStutter_local_sim_implies_gsim. 3: eapply init.
+    instantiate (1:= fun o0 => @epsilon _ wf_tgt_inhabited (fun o1 => wf_tgt.(lt) o0 o1)).
+    { i. hexploit (@epsilon_spec _ wf_tgt_inhabited (fun o1 => wf_tgt.(lt) t o1)); eauto. }
+    instantiate (1:=wf_stt).
 
-
-
-  (* Definition mod_prog_wf (m: Mod.t) (p: program) := *)
-  (*   Forall (fun '(fn, args) => match Mod.funs m fn with Some _ => True | _ => False end) p. *)
-
-  (* Fixpoint mod_prog2ths_ (m: Mod.t) (p: program) (k: Th.key): *)
-  (*   option (list (Th.key * @thread (Mod.ident m) (sE (Mod.state m)) Val))%type := *)
-  (*   match p with *)
-  (*   | (fn, args) :: tl => *)
-  (*       match Mod.funs m fn with *)
-  (*       | Some ktr => match mod_prog2ths_ m tl (S k) with *)
-  (*                    | Some ths => Some ((k, ktr args) :: ths) *)
-  (*                    | _ => None *)
-  (*                    end *)
-  (*       | _ => None *)
-  (*       end *)
-  (*   | _ => Some [] *)
-  (*   end. *)
-
-  (* Definition mod_prog2ths (m: Mod.t) (p: program): option (@threads (Mod.ident m) (sE (Mod.state m)) Val) := *)
-  (*   match mod_prog2ths_ m p 0 with *)
-  (*   | Some lths => Some (NatMapP.of_list lths) *)
-  (*   | _ => None *)
-  (*   end. *)
-
-  (* Definition ModSimStutter.local_sim_threads_list *)
-  (*            (m1 m2: Mod.t) *)
-  (*            (lths_src: list (Th.key * @thread (Mod.ident m1) (sE (Mod.state m1)) Val)%type)  *)
-  (*            (lths_tgt: list (Th.key * @thread (Mod.ident m2) (sE (Mod.state m2)) Val)%type) *)
-  (*            (wf_src wf_tgt: WF) *)
-  (*            (world: Type) (world_le: world -> world -> Prop) *)
-  (*            I *)
-  (*   := *)
-  (*   List.Forall2 *)
-  (*     (fun '(t1, src) '(t2, tgt) => (t1 = t2) /\ (ModSimStutter.local_sim ( world_le I (@eq Val) src tgt)) *)
-  (*     lths_src lths_tgt. *)
-
-  (* Lemma mod_prog_wf_prog_ex_ths_ *)
-  (*       m p *)
-  (*       (WF: mod_prog_wf m p) *)
-  (*       (MS: ModSim.mod_sim *)
-  (*   : *)
-  (*   forall n, exists ths, (mod_prog2ths_ m p n = Some ths). *)
-  (* Proof. *)
-  (*   induction WF; i. *)
-  (*   { eexists. ss. } *)
-  (*   ss. des_ifs. *)
-  (*   { eexists. eauto. } *)
-  (*   { exfalso. specialize (IHWF (S n)). des. clarify. } *)
-  (* Qed. *)
-
-  (* Lemma mod_prog_wf_prog_ex_ths *)
-  (*       m p *)
-  (*       (WF: mod_prog_wf m p) *)
-  (*   : *)
-  (*   exists ths, mod_prog2ths m p = Some ths. *)
-  (* Proof. *)
-  (*   unfold mod_prog2ths. hexploit mod_prog_wf_prog_ex_ths_; eauto. i; des. eexists. rewrite H. eauto. *)
-  (* Qed. *)
-
-  (* Definition mod_prog_improves (m_src m_tgt: Mod.t) sched (p: program) := *)
-  (*   forall tid, *)
-  (*     let oths_src := mod_prog2ths m_src p in *)
-  (*     let oths_tgt := mod_prog2ths m_tgt p in *)
-  (*     match oths_src, oths_tgt with *)
-  (*     | Some ths_src, Some ths_tgt => *)
-  (*         improves (interp_mod m_src tid ths_src sched_nondet) (interp_mod m_tgt tid ths_tgt sched) *)
-  (*     | _, _ => False *)
-  (*     end. *)
-
-  (* Theorem  *)
 
 
 End ADEQ.
