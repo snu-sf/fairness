8c51a085712c994461e7986ff1232a9aee4a2fa1
minkiminki <minki.cho@sf.snu.ac.kr>
2022-09-22 16:57:00 +0900

    SC Memory with weak cas

--- src/SCM.v
+++ src/SCM.v
@@ -8,6 +8,8 @@ Set Implicit Arguments.
 Module SCMem.
   Definition pointer := (nat * nat)%type.
 
+  Let ident := pointer.
+
   Variant val: Type :=
     | val_nat (n: nat)
     | val_ptr (p: pointer)
@@ -19,22 +21,30 @@ Module SCMem.
     | val_ptr p => Some p
     end.
 
+  Definition ptr_eq_dec (p0 p1: pointer): {p0 = p1} + {p0 <> p1}.
+  Proof.
+    destruct p0 as [blk0 ofs0], p1 as [blk1 ofs1].
+    destruct (PeanoNat.Nat.eq_dec blk0 blk1).
+    - destruct (PeanoNat.Nat.eq_dec ofs0 ofs1).
+      + left. f_equal; assumption.
+      + right. ii. inject H. apply n. reflexivity.
+    - right. ii. inject H. apply n. reflexivity.
+  Qed.
+
   Definition val_null: val := val_nat 0.
 
   Definition val_eq_dec (v0 v1: val): {v0 = v1} + {v0 <> v1}.
   Proof.
-    destruct v0 as [n0|[blk0 ofs0]], v1 as [n1|[blk1 ofs1]].
+    destruct v0 as [n0|p0], v1 as [n1|p1].
     - destruct (PeanoNat.Nat.eq_dec n0 n1).
       + left. f_equal; assumption.
       + right. ii. inject H. apply n. reflexivity.
     - right. ii. inject H.
     - right. ii. inject H.
-    - destruct (PeanoNat.Nat.eq_dec blk0 blk1).
-      + destruct (PeanoNat.Nat.eq_dec ofs0 ofs1).
-        * left. f_equal. f_equal; assumption.
-        * right. ii. inject H. apply n. reflexivity.
+    - destruct (ptr_eq_dec p0 p1).
+      + left. f_equal; assumption.
       + right. ii. inject H. apply n. reflexivity.
-  Defined.
+  Qed.
 
   Definition val_add (v: val) (n: nat): val :=
     match v with
@@ -49,28 +59,28 @@ Module SCMem.
         next_block: nat;
       }.
 
+  Definition has_permission (m: t) (ptr: pointer): bool :=
+    let (blk, ofs) := ptr in
+    if (m.(contents) blk ofs) then true else false.
+
   Definition val_valid (m: t) (v: val): bool :=
     match v with
     | val_nat _ => true
-    | val_ptr (blk, ofs) =>
-        if (m.(contents) blk ofs) then true else false
+    | val_ptr p =>
+        if (has_permission m p) then true else false
     end.
 
   Definition val_compare (m: t) (v0 v1: val): option bool :=
     match v0, v1 with
     | val_nat n0, val_nat n1 => Some (if (PeanoNat.Nat.eq_dec n0 n1) then true else false)
-    | val_nat n, val_ptr (blk, ofs)
-    | val_ptr (blk, ofs), val_nat n =>
-        if (m.(contents) blk ofs)
+    | val_nat n, val_ptr p
+    | val_ptr p, val_nat n =>
+        if (has_permission m p)
         then Some (if (PeanoNat.Nat.eq_dec n 0) then true else false)
         else None
-    | val_ptr (blk0, ofs0), val_ptr (blk1, ofs1) =>
-        if (m.(contents) blk0 ofs0) then
-          if (m.(contents) blk1 ofs1) then
-            Some (if (PeanoNat.Nat.eq_dec blk0 blk1)
-                  then if (PeanoNat.Nat.eq_dec ofs0 ofs1) then true else false
-                  else false)
-          else None
+    | val_ptr p0, val_ptr p1 =>
+        if (has_permission m p0 && has_permission m p1)%bool then
+          Some (if (ptr_eq_dec p0 p1) then true else false)
         else None
     end.
 
@@ -130,7 +140,7 @@ Module SCMem.
     end.
 
   Definition alloc_fun:
-    ktree (((@eventE void) +' cE) +' sE t) nat val :=
+    ktree (((@eventE ident) +' cE) +' sE t) nat val :=
     fun sz =>
       m <- trigger (@Get _);;
       let (m, v) := alloc m sz in
@@ -139,7 +149,7 @@ Module SCMem.
   .
 
   Definition store_fun:
-    ktree (((@eventE void) +' cE) +' sE t) (val * val) unit :=
+    ktree (((@eventE ident) +' cE) +' sE t) (val * val) unit :=
     fun '(vptr, v) =>
       p <- unwrap (unwrap_ptr vptr);;
       m <- trigger (@Get _);;
@@ -149,7 +159,7 @@ Module SCMem.
   .
 
   Definition load_fun:
-    ktree (((@eventE void) +' cE) +' sE t) val val :=
+    ktree (((@eventE ident) +' cE) +' sE t) val val :=
     fun vptr =>
       p <- unwrap (unwrap_ptr vptr);;
       m <- trigger (@Get _);;
@@ -158,7 +168,7 @@ Module SCMem.
   .
 
   Definition faa_fun:
-    ktree (((@eventE void) +' cE) +' sE t) (val * nat) val :=
+    ktree (((@eventE ident) +' cE) +' sE t) (val * nat) val :=
     fun '(vptr, addend) =>
       p <- unwrap (unwrap_ptr vptr);;
       m <- trigger (@Get _);;
@@ -168,13 +178,32 @@ Module SCMem.
   .
 
   Definition cas_fun:
-    ktree (((@eventE void) +' cE) +' sE t) (val * val * val) bool :=
+    ktree (((@eventE ident) +' cE) +' sE t) (val * val * val) bool :=
     fun '(vptr, v_old, v_new) =>
       p <- unwrap (unwrap_ptr vptr);;
       m <- trigger (@Get _);;
       '(m, b) <- unwrap (cas m p v_old v_new);;
       _ <- trigger (Put m);;
       Ret b
+  .
+
+  Definition cas_weak_fun:
+    ktree (((@eventE ident) +' cE) +' sE t) (val * val * val) bool :=
+    fun '(vptr, v_old, v_new) =>
+      p <- unwrap (unwrap_ptr vptr);;
+      m <- trigger (@Get _);;
+      b <- trigger (Choose bool);;
+      if (b: bool)
+      then
+        '(m, b) <- unwrap (cas m p v_old v_new);;
+        _ <- trigger (Put m);;
+        Ret b
+      else
+        if has_permission m p
+        then
+          _ <- trigger (Fair (fun i => if ptr_eq_dec i p then Flag.fail else Flag.success));;
+          Ret false
+        else UB
   .
 
   Definition mod: Mod.t :=
@@ -184,6 +213,7 @@ Module SCMem.
                      ("store", Mod.wrap_fun store_fun);
                      ("load", Mod.wrap_fun load_fun);
                      ("faa", Mod.wrap_fun faa_fun);
-                     ("cas", Mod.wrap_fun cas_fun)
+                     ("cas", Mod.wrap_fun cas_fun);
+                     ("cas_weak", Mod.wrap_fun cas_weak_fun)
       ]).
 End SCMem.
