7ceca0b2bf7e5dc4b39176eb39ad1c8ba0e52545
minkiminki <minki.cho@sf.snu.ac.kr>
2022-11-05 16:42:24 +0900

    Update defaultI

--- src/StateRA.v
+++ src/StateRA.v
@@ -3,7 +3,7 @@ From Paco Require Import paco.
 From Fairness Require Import PCM ITreeLib pind.
 Require Import Program.
 From Fairness Require Import IProp IPM.
-From Fairness Require Import PCM MonotonePCM ThreadsRA ModSim FairBeh.
+From Fairness Require Import PCM MonotonePCM NatMapRA ModSim FairBeh.
 
 Set Implicit Arguments.
 
@@ -44,6 +44,7 @@ Section UPD.
   Qed.
 End UPD.
 
+From Fairness Require Import FairRA.
 
 Section INVARIANT.
   Variable state_src: Type.
@@ -51,36 +52,60 @@ Section INVARIANT.
 
   Variable ident_src: ID.
   Variable ident_tgt: ID.
-  Variable wf_src: WF.
 
   Definition stateSrcRA: URA.t := Auth.t (Excl.t state_src).
   Definition stateTgtRA: URA.t := Auth.t (Excl.t state_tgt).
-  Definition identSrcRA: URA.t := Auth.t (Excl.t (imap ident_src wf_src)).
-  Definition identTgtRA: URA.t := Auth.t (Excl.t (imap ident_tgt nat_wf)).
-  Definition identThsRA: URA.t := Auth.t (Excl.t (imap thread_id nat_wf)).
+  Definition identSrcRA: URA.t := FairRA.srct ident_src.
+  Definition identTgtRA: URA.t := FairRA.tgtt ident_tgt.
+  Definition ThreadRA: URA.t := Auth.t (NatMapRA.t unit).
+  Definition ArrowRA: URA.t :=
+    Region.t ((sum_tid ident_tgt) * nat * Ord.t * Qp * nat).
+  Definition EdgeRA: URA.t :=
+    Region.t (nat * nat * Ord.t).
 
   Context `{MONORA: @GRA.inG monoRA Σ}.
-  Context `{THSRA: @GRA.inG ths_RA Σ}.
+  Context `{THDRA: @GRA.inG ThreadRA Σ}.
   Context `{STATESRC: @GRA.inG stateSrcRA Σ}.
   Context `{STATETGT: @GRA.inG stateTgtRA Σ}.
   Context `{IDENTSRC: @GRA.inG identSrcRA Σ}.
   Context `{IDENTTGT: @GRA.inG identTgtRA Σ}.
-  Context `{IDENTTHS: @GRA.inG identThsRA Σ}.
+  Context `{OBLGRA: @GRA.inG ObligationRA.t Σ}.
+  Context `{ARROWRA: @GRA.inG ArrowRA Σ}.
+  Context `{EDGERA: @GRA.inG EdgeRA Σ}.
+  Context `{ONESHOTRA: @GRA.inG (@FiniteMap.t (OneShot.t unit)) Σ}.
 
-  Definition default_I: TIdSet.t -> (@imap ident_src wf_src) -> (@imap (sum_tid ident_tgt) nat_wf) -> state_src -> state_tgt -> iProp :=
+  Definition default_I: TIdSet.t -> (@imap ident_src owf) -> (@imap (sum_tid ident_tgt) nat_wf) -> state_src -> state_tgt -> iProp :=
     fun ths im_src im_tgt st_src st_tgt =>
-      (own_threads_black ths)
+      (OwnM (Auth.black (Some ths: (NatMapRA.t unit)): ThreadRA))
         **
         (OwnM (Auth.black (Excl.just st_src: @Excl.t state_src): stateSrcRA))
         **
         (OwnM (Auth.black (Excl.just st_tgt: @Excl.t state_tgt): stateTgtRA))
         **
-        (OwnM (Auth.black (Excl.just im_src: @Excl.t (imap ident_src wf_src)): identSrcRA))
+        (FairRA.sat_source im_src)
         **
-        (OwnM (Auth.black (Excl.just (snd (imap_proj_id im_tgt)): @Excl.t (imap ident_tgt nat_wf)): identTgtRA))
+        (FairRA.sat_target im_tgt ths)
         **
-        (OwnM (Auth.black (Excl.just (fst (imap_proj_id im_tgt)): @Excl.t (imap thread_id nat_wf)): identThsRA))
+        (ObligationRA.edges_sat)
+        **
+        (ObligationRA.arrows_sat (Id := sum_tid ident_tgt))
   .
+
+  Definition own_thread (tid: thread_id): iProp :=
+    (OwnM (Auth.white (NatMapRA.singleton tid tt: NatMapRA.t unit): ThreadRA)).
+
+  Lemma own_thread_unique tid0 tid1
+    :
+    (own_thread tid0)
+      -∗
+      (own_thread tid1)
+      -∗
+      ⌜tid0 <> tid1⌝.
+  Proof.
+    iIntros "OWN0 OWN1". iCombine "OWN0 OWN1" as "OWN".
+    iOwnWf "OWN". ur in H. apply NatMapRA.singleton_unique in H.
+    iPureIntro. auto.
+  Qed.
 
   Lemma default_I_update_st_src ths im_src im_tgt st_src0 st_tgt st_src' st_src1
     :
@@ -90,8 +115,8 @@ Section INVARIANT.
       -∗
       #=> (OwnM (Auth.white (Excl.just st_src1: @Excl.t state_src)) ** default_I ths im_src im_tgt st_src1 st_tgt).
   Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iPoseProof (black_white_update with "H1 OWN") as "> [H1 OWN]".
+    unfold default_I. iIntros "[[[[[[A B] C] D] E] F] G] OWN".
+    iPoseProof (black_white_update with "B OWN") as "> [B OWN]".
     iModIntro. iFrame.
   Qed.
 
@@ -103,153 +128,83 @@ Section INVARIANT.
       -∗
       #=> (OwnM (Auth.white (Excl.just st_tgt1: @Excl.t state_tgt)) ** default_I ths im_src im_tgt st_src st_tgt1).
   Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iPoseProof (black_white_update with "H2 OWN") as "> [H2 OWN]".
+    unfold default_I. iIntros "[[[[[[A B] C] D] E] F] G] OWN".
+    iPoseProof (black_white_update with "C OWN") as "> [C OWN]".
     iModIntro. iFrame.
   Qed.
 
-  Lemma default_I_update_ident_src ths im_src0 im_tgt st_src st_tgt im_src' im_src1
-    :
-    (default_I ths im_src0 im_tgt st_src st_tgt)
-      -∗
-      (OwnM (Auth.white (Excl.just im_src': @Excl.t (imap ident_src wf_src)): identSrcRA))
-      -∗
-      #=> (OwnM (Auth.white (Excl.just im_src1: @Excl.t (imap ident_src wf_src))) ** default_I ths im_src1 im_tgt st_src st_tgt).
-  Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iPoseProof (black_white_update with "H3 OWN") as "> [H3 OWN]".
-    iModIntro. iFrame.
-  Qed.
-
-  Lemma default_I_update_ident_tgt ths im_src im_tgt0 st_src st_tgt im_tgt' im_tgt1
-    :
-    (default_I ths im_src im_tgt0 st_src st_tgt)
-      -∗
-      (OwnM (Auth.white (Excl.just im_tgt': @Excl.t (imap ident_tgt nat_wf)): identTgtRA))
-      -∗
-      #=> (OwnM (Auth.white (Excl.just im_tgt1: @Excl.t (imap ident_tgt nat_wf))) ** default_I ths im_src (imap_sum_id (fst (imap_proj_id im_tgt0), im_tgt1)) st_src st_tgt).
-  Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iPoseProof (black_white_update with "H4 OWN") as "> [H4 OWN]".
-    ss. iModIntro. iFrame.
-  Qed.
-
-  Lemma default_I_update_ident_ths ths im_src im_tgt0 st_src st_tgt im_tgt' im_tgt1
-    :
-    (default_I ths im_src im_tgt0 st_src st_tgt)
-      -∗
-      (OwnM (Auth.white (Excl.just im_tgt': @Excl.t (imap thread_id nat_wf)): identThsRA))
-      -∗
-      #=> (OwnM (Auth.white (Excl.just im_tgt1: @Excl.t (imap thread_id nat_wf))) ** default_I ths im_src (imap_sum_id (im_tgt1, snd (imap_proj_id im_tgt0))) st_src st_tgt).
-  Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iPoseProof (black_white_update with "H5 OWN") as "> [H5 OWN]".
-    ss. iModIntro. iFrame.
-  Qed.
-
-  Lemma default_I_get_st_src ths im_src im_tgt st_src st_tgt st_src'
-    :
-    (default_I ths im_src im_tgt st_src st_tgt)
-      -∗
-      (OwnM (Auth.white (Excl.just st_src': @Excl.t state_src): stateSrcRA))
-      -∗
-      ⌜st_src = st_src'⌝.
-  Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iApply (black_white_equal with "H1 OWN").
-  Qed.
-
-  Lemma default_I_get_st_tgt ths im_src im_tgt st_src st_tgt st_tgt'
-    :
-    (default_I ths im_src im_tgt st_src st_tgt)
-      -∗
-      (OwnM (Auth.white (Excl.just st_tgt': @Excl.t state_tgt): stateTgtRA))
-      -∗
-      ⌜st_tgt = st_tgt'⌝.
-  Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iApply (black_white_equal with "H2 OWN").
-  Qed.
-
-  Lemma default_I_get_ident_src ths im_src im_tgt st_src st_tgt im_src'
-    :
-    (default_I ths im_src im_tgt st_src st_tgt)
-      -∗
-      (OwnM (Auth.white (Excl.just im_src': @Excl.t (imap ident_src wf_src)): identSrcRA))
-      -∗
-      ⌜im_src = im_src'⌝.
-  Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iApply (black_white_equal with "H3 OWN").
-  Qed.
-
-  Lemma default_I_get_ident_tgt ths im_src im_tgt st_src st_tgt im_tgt'
-    :
-    (default_I ths im_src im_tgt st_src st_tgt)
-      -∗
-      (OwnM (Auth.white (Excl.just im_tgt': @Excl.t (imap ident_tgt nat_wf)): identTgtRA))
-      -∗
-      ⌜snd (imap_proj_id im_tgt) = im_tgt'⌝.
-  Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iApply (black_white_equal with "H4 OWN").
-  Qed.
-
-  Lemma default_I_get_ident_ths ths im_src im_tgt st_src st_tgt im_tgt'
-    :
-    (default_I ths im_src im_tgt st_src st_tgt)
-      -∗
-      (OwnM (Auth.white (Excl.just im_tgt': @Excl.t (imap thread_id nat_wf)): identThsRA))
-      -∗
-      ⌜fst (imap_proj_id im_tgt) = im_tgt'⌝.
-  Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] OWN".
-    iApply (black_white_equal with "H5 OWN").
-  Qed.
-
-  Lemma default_I_thread_alloc ths0 im_src im_tgt st_src st_tgt ths'
-        tid ths1
+  Lemma default_I_thread_alloc ths0 im_src im_tgt0 st_src st_tgt
+        tid ths1 im_tgt1
         (THS: TIdSet.add_new tid ths0 ths1)
+        (TID_TGT : fair_update im_tgt0 im_tgt1 (sum_fmap_l (fun i => if tid_dec i tid then Flag.success else Flag.emp)))
     :
-    (default_I ths' im_src im_tgt st_src st_tgt)
+    (default_I ths0 im_src im_tgt0 st_src st_tgt)
       -∗
-      (own_threads_white ths0)
-      -∗
-      #=> (own_thread tid ** own_threads_white ths1 ** default_I ths1 im_src im_tgt st_src st_tgt).
+      #=> (own_thread tid ** ObligationRA.duty (inl tid) [] ** default_I ths1 im_src im_tgt1 st_src st_tgt).
   Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] H6".
-    iPoseProof (threads_alloc with "H0 H6") as "> [[BLACK WHITE] TH]".
+    unfold default_I. iIntros "[[[[[[A B] C] D] E] F] G]".
+    iPoseProof (OwnM_Upd with "A") as "> [A TH]".
+    { apply Auth.auth_alloc.
+      eapply (@NatMapRA.add_local_update unit ths0 tid tt). inv THS. auto.
+    }
+    iPoseProof (FairRA.target_add_thread with "E") as "> [E BLACK]"; [eauto|eauto|].
+    iModIntro. inv THS. iFrame.
+    unfold ObligationRA.duty. iExists [], 1%Qp. iFrame. ss.
+  Qed.
+
+  Lemma default_I_update_ident_thread ths im_src im_tgt0 st_src st_tgt
+        tid im_tgt1 l
+        (UPD: fair_update im_tgt0 im_tgt1 (sum_fmap_l (tids_fmap tid ths)))
+    :
+    (default_I ths im_src im_tgt0 st_src st_tgt)
+      -∗
+      (ObligationRA.duty (inl tid) l ** ObligationRA.tax l)
+      -∗
+      #=> (ObligationRA.duty (inl tid) l ** FairRA.white_thread (_Id:=_) ** default_I ths im_src im_tgt1 st_src st_tgt).
+  Proof.
+    unfold default_I. iIntros "[[[[[[A B] C] D] E] F] G] DUTY".
+    iPoseProof (ObligationRA.target_update_thread with "E DUTY G") as "> [G [[E BLACK] WHITE]]"; [eauto|].
+    iModIntro. iFrame.
+  Qed.
+
+  Lemma default_I_update_ident_target lf ls
+        ths im_src im_tgt0 st_src st_tgt
+        fm im_tgt1
+        (UPD: fair_update im_tgt0 im_tgt1 (sum_fmap_r fm))
+        (SUCCESS: forall i (IN: fm i = Flag.success), List.In i (List.map fst ls))
+        (FAIL: forall i (IN: List.In i lf), fm i = Flag.fail)
+        (NODUP: List.NoDup lf)
+    :
+    (default_I ths im_src im_tgt0 st_src st_tgt)
+      -∗
+      (list_prop_sum (fun '(i, l) => ObligationRA.duty (inr i) l ** ObligationRA.tax l) ls)
+      -∗
+      #=> ((list_prop_sum (fun '(i, l) => ObligationRA.duty (inr i) l) ls)
+             **
+             (list_prop_sum (fun i => FairRA.white (Id:=_) (inr i) 1) lf)
+             **
+             default_I ths im_src im_tgt1 st_src st_tgt).
+  Proof.
+    unfold default_I. iIntros "[[[[[[A B] C] D] E] F] G] DUTY".
+    iPoseProof (ObligationRA.target_update with "E DUTY G") as "> [G [[E BLACK] WHITE]]".
+    { eauto. }
+    { eauto. }
+    { eauto. }
     { eauto. }
     iModIntro. iFrame.
   Qed.
 
-  Lemma default_I_thread_dealloc ths0 im_src im_tgt st_src st_tgt ths'
-        tid
+  Lemma arrows_sat_sub ths im_src im_tgt st_src st_tgt
     :
-    (default_I ths' im_src im_tgt st_src st_tgt)
-      -∗
-      (own_threads_white ths0)
-      -∗
-      (own_thread tid)
-      -∗
-      ∃ ths1, ⌜NatMap.remove tid ths0 = ths1⌝ ** #=> (own_threads_white ths1 ** default_I ths1 im_src im_tgt st_src st_tgt).
+    ⊢ SubIProp (ObligationRA.arrows_sat (Id := sum_tid ident_tgt)) (default_I ths im_src im_tgt st_src st_tgt).
   Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] THS TH".
-    iPoseProof (threads_dealloc with "H0 THS TH") as (ths1) "[% H0]".
-    iExists _. iSplit; eauto. iMod "H0". iModIntro.
-    iDestruct "H0" as "[H0 H6]". iFrame.
+    iIntros "[[[[[[A B] C] D] E] F] G]". iModIntro. iFrame. auto.
   Qed.
 
-  Lemma default_I_ths_eq ths im_src im_tgt st_src st_tgt ths'
+  Lemma edges_sat_sub ths im_src im_tgt st_src st_tgt
     :
-    (default_I ths im_src im_tgt st_src st_tgt)
-      -∗
-      (own_threads_white ths')
-      -∗
-      ⌜ths = ths'⌝.
+    ⊢ SubIProp (ObligationRA.edges_sat) (default_I ths im_src im_tgt st_src st_tgt).
   Proof.
-    unfold default_I. iIntros "[[[[[H0 H1] H2] H3] H4] H5] THS".
-    iApply (threads_eq with "H0 THS").
+    iIntros "[[[[[[A B] C] D] E] F] G]". iModIntro. iFrame. auto.
   Qed.
-
 End INVARIANT.
