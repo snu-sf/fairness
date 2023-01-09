1bf97c2276b10cdc6bf818a8e5e71cdc1d7de828
damhiya <damhiya@gmail.com>
2023-01-09 17:50:40 +0900

    add FancyUpdate.v

---
+++ src/logic/FancyUpdate.v
@@ -0,0 +1,11 @@
+From stdpp Require Import coPset.
+From Fairness Require Import PCM IProp IPM.
+
+Section FANCY_UPDATE.
+
+  Context `{Σ : GRA.t}.
+
+  Definition FUpd (E1 E2 : coPset) (P : iProp) : iProp.
+  Admitted.
+
+End FANCY_UPDATE.
