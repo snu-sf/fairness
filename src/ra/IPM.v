6d164b9bd2d08ba8442181e8d8be376a16e7c04d
Janggun Lee <leesisi123@naver.com>
2024-07-21 00:39:24 +0900

    Remove redunddent TC instance.

--- src/ra/IPM.v
+++ src/ra/IPM.v
@@ -197,15 +197,6 @@ Section IPM.
     rr. uipropall. i. rr. uipropall. rr in H. uipropall. des; [|done].
     rr in H. uipropall. rr in H. uipropall.
   Qed.
-  Global Instance elim_modal_except_0 p (P Q : iProp) :
-    ElimModal True p p (◇ P) P Q Q.
-  Proof.
-    rewrite /ElimModal. iIntros (_) "[P Q]".
-    iApply "Q".
-    destruct p; simpl.
-    1: iDestruct "P" as "#P"; iModIntro.
-    all: iDestruct (is_except_0 with "P") as "$".
-  Qed.
 
   Global Instance iProp_pure_forall: BiPureForall iProp.
   Proof.
